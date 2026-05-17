13c1b985b77ac04712c532ff79c7e55338e048b0
dmcagro <soladiamond.sd@gmail.com>
2026-05-17 16:56:11 +0100

    journal project done

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -20,6 +20,42 @@ class Program
 
             Console.Write("Choose an option: ");
             choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+
+                Console.WriteLine($"\nPrompt: {prompt}");
+                Console.Write("> ");
+                string response = Console.ReadLine();
+
+                string date = DateTime.Now.ToShortDateString();
+
+                Entry entry = new Entry();
+                entry._date = date;
+                entry._promptText = prompt;
+                entry._entryText = response;
+
+                journal.AddEntry(entry);
+            }
+            else if (choice == 2)
+            {
+                journal.DisplayAll();
+            }
+            else if (choice == 3)
+            {
+                Console.Write("Enter filename to save: ");
+                string filename = Console.ReadLine();
+
+                journal.SaveToFile(filename);
+            }
+            else if (choice == 4)
+            {
+                Console.Write("Enter filename to load: ");
+                string filename = Console.ReadLine();
+
+                journal.LoadFromFile(filename);
+            }
         }
     }
 }
