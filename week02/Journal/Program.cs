e269e1942976f0e71fbc972862653de397ac0ead
dmcagro <soladiamond.sd@gmail.com>
2026-05-17 12:41:31 +0100

    codes made

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        int choice = 0;
+
+        while (choice != 5)
+        {
+            Console.WriteLine("\nJournal Menu");
+            Console.WriteLine("1. Write New Entry");
+            Console.WriteLine("2. Display Journal");
+            Console.WriteLine("3. Save Journal");
+            Console.WriteLine("4. Load Journal");
+            Console.WriteLine("5. Exit");
+
+            Console.Write("Choose an option: ");
+            choice = int.Parse(Console.ReadLine());
+        }
     }
 }
